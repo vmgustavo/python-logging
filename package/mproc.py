cfcd767ee069b8e298c86898534e1548ba9d8755
Gustavo Maia <vm.gustavo31@gmail.com>
2020-01-10 14:24:23 -0300

    add logger propagate

--- package/mproc.py
+++ package/mproc.py
@@ -11,8 +11,11 @@ def func(x):
 
 def mproc():
     logger.info('start mproc')
+    logger.propagate = False
+    logger.info('this will not be logged')
     param = [[elem] for elem in range(10)]
     with multiprocessing.Pool(processes=2) as pool:
         res = pool.starmap(func=func, iterable=param)
+    logger.propagate = True
     logger.info('end mproc')
     return res
